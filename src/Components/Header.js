ef532c52b5db99153033aa58e550cdc1cfee7b86
Parvesh Barak <pbarak6@gmail.com>
2023-06-26 17:39:05 +0530

    logo Link updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   }
   return (
     <header className='header'>
-      <Link to='#' className='logo'>
+      <Link to='/' className='logo'>
         {' '}
         <span>iron</span>Horse{' '}
       </Link>
