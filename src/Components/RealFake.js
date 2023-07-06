f9e2e4d5351a37e50368b6018fb855c8908d45c2
Parvesh Barak <pbarak6@gmail.com>
2023-07-06 17:33:43 +0530

    realFake page added

--- src/Components/RealFake.js
+++ src/Components/RealFake.js
@@ -2,9 +2,67 @@ import React from 'react'
 
 const RealFake = () => {
   return (
-    <div>
-      Hey hey aa gye na nyee page p..hihihihi
-    </div>
+    <section className='realFake' id='realFake'>
+      <h1 className='heading'>
+        <span>Real vs Fake</span>
+      </h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='column'>
+          <img src= '/images/f-img-1.jpg' alt = "hello1"/>
+          <h3>Real ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+          <div className='column'>
+          <img src= '/images/f-img-2.jpg' alt = "hello1"/>
+          <h3>Fake ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='column'>
+          <img src= '/images/f-img-1.jpg' alt = "hello1"/>
+          <h3>Real ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+          <div className='column'>
+          <img src= '/images/f-img-2.jpg' alt = "hello1"/>
+          <h3>Fake ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='column'>
+          <img src= '/images/f-img-1.jpg' alt = "hello1"/>
+          <h3>Real ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+          <div className='column'>
+          <img src= '/images/f-img-2.jpg' alt = "hello1"/>
+          <h3>Fake ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='column'>
+          <img src= '/images/f-img-1.jpg' alt = "hello1"/>
+          <h3>Real ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+          <div className='column'>
+          <img src= '/images/f-img-2.jpg' alt = "hello1"/>
+          <h3>Fake ironHorse</h3>
+          <p>here comes the blah blah blah description</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
