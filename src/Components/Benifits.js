6adfac83e4c3ca0c8fe6856630d823ebfdeba836
Parvesh Barak <pbarak6@gmail.com>
2023-07-09 18:37:19 +0530

    highlights updated

--- src/Components/Benifits.js
+++ src/Components/Benifits.js
@@ -37,10 +37,9 @@ const Benifits = () => {
       >
         <div className='box'>
           <div className='image'>
-            <img src='images/f-img-1.jpg' alt='' />
+            <img src='images/h1.jpeg' alt='' />
           </div>
           <div className='content'>
-            <img src='images/icon-1.png' alt='' />
             <h3>Super micronised winny</h3>
             <p>
               Iron horse winny is painless, smooth and easy to inject. There
@@ -51,30 +50,15 @@ const Benifits = () => {
 
         <div className='box second'>
           <div className='image'>
-            <img src='images/f-img-2.jpg' alt='' />
+            <img src='images/h2.jpeg' alt='' />
           </div>
           <div className='content'>
-            <img src='images/hey.none' alt='' />
             <h3>No lump problem </h3>
             <p>
               All our injections are painless and smooth . Iron horse guarantees
               you there will be no lump formation after injecting our projects .
               Make sure to clean injection site with alcohol swap before
               injecting
-            </p>
-          </div>
-        </div>
-
-        <div className='box'>
-          <div className='image'>
-            <img src='images/f-img-3.jpg' alt='' />
-          </div>
-          <div className='content'>
-            <img src='images/hey2.png' alt='' />
-            <h3>From athletes to athletes </h3>
-            <p>
-              Iron horse winny is painless , smooth and easy to inject . There
-              will be no syringe blockage problem{' '}
             </p>
           </div>
         </div>
